1888b1d5996be59ea6f4beb531d9e83ebbee859f
Andrew Smith <smith.andrew.l@gmail.com>
2016-02-25 20:55:24 -0600

    Changed main file name to flare_demo.  Moved code/pseudocode from multiple files into flare.nim
    Added Life and Property types to flare.nim

--- flare.nim
+++ flare.nim
@@ -1,27 +1,99 @@
+
+
+############ Particle ##########################
+discard """
+    Emitter
+    Particle
+    ParticlePool
+
+  Emitter (initialized with an object pool)
+    physics: Physics
+    
+    Velocity:   Property
+    Rotation: Property
+    Size:    Property
+    Color:   Property
+    Alpha:   Property
+    
+    maxParticles: int
+    curParticles: int
+
+    image: texture/image
+
+    methods:
+        draw()
+        update()
+
+  Particle
+    Physics
+    tint:  Color
+    Life: life
+    methods:
+        draw()
+        update()
+
+ParticlePool
+  constructror(number)
+  obtain
+  release
+"""
+
+type
+  Life* = ref object of RootObj
+    IsAlive: bool
+    Age:     int
+    Ttl:     int
+  
+    
+proc update*(life: Life) =
+  if life.IsAlive:
+    if life.Age > life.Ttl:
+      life.IsAlive = false
+    else:
+      life.Age = life.Age + 1
+
+proc newLife*(alive: bool; ttl: int): Life =
+  let life: Life = new(Life)
+
+  life.IsAlive = alive
+  life.Age     = 0
+  life.Ttl     = ttl
+
+  result = life
+
+
+######### Util ###########
+
+type
+  Property*[T] = ref object of RootObj
+    startValue: T
+    endValue:   T
+    variance:   float
+
+proc newProperty*[T](startValue: T, endValue: T, variance: float): Property[T] =
+  let property: Property = new(Property)
+
+  property.startValue = startValue
+  property.endValue   = endValue
+  property.variance   = variance
+
+  result = property
+
+#ObjectPool
+
+
+##### Physics ############
 import csfml
 
-const
-  WINDOW_TITLE  = "Flare"
-  WINDOW_WIDTH  = 800
-  WINDOW_HEIGHT = 600
+type
+  Physics* = ref object of RootObj
+    location:        Vector2f
+    velocity:        Vector2f
+    acceleration:    Vector2f
+    angularVelocity: float
+    angularAcc:      float
+    rotation:        float
 
-var window = new_RenderWindow(video_mode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE)
+proc draw*(phys: Physics)   = discard 
+proc update*(phys: Physics) = discard 
 
-window.vertical_sync_enabled = true
-
-while window.open:
-    var event: Event
-    while window.poll_event(event):
-        case event.kind
-          of EventType.Closed:
-            window.close()
-          of EventType.KeyPressed:
-            case event.key.code
-              of KeyCode.Q:
-                window.close()
-              else:
-                discard " "
-          else: discard
-
-    window.clear color(0, 0, 0)    
-    window.display()
--- flare.nimble
+++ flare.nimble
@@ -5,7 +5,7 @@ author = "Andrew Smith"
 description = "A particle effects engine." 
 license = "MIT"
 
-bin = "flare"
+bin = "flare_demo"
 
 [Deps] 
 Requires: "nim >= 0.10.0, csfml" 
