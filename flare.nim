4703dbde5f3aa573fa304c0956aa3fefa33a8245
Andrew Smith <smith.andrew.l@gmail.com>
2016-03-03 21:00:10 -0600

    Made the code more idiomatic

--- flare.nim
+++ flare.nim
@@ -1,9 +1,4 @@
-import csfml
-import mersenne
-import math
-import times
-
-######### Util ###########
+import csfml, mersenne, math, times, sequtils
 
 type
   Property* = ref object of RootObj
@@ -12,15 +7,11 @@ type
     variance:   float
 
 proc newProperty*(startValue: float, endValue: float, variance: float): Property =
-  let property: Property = new(Property)
+  result = new(Property)
 
-  property.startValue = startValue
-  property.endValue   = endValue
-  property.variance   = variance
-
-  result = property
-
-##### Physics ############
+  result.startValue = startValue
+  result.endValue   = endValue
+  result.variance   = variance
 
 type
   Physics* = ref object of RootObj
@@ -32,14 +23,11 @@ proc update*(phys: Physics) =
   phys.location = phys.location + vec2(phys.speed * cos(phys.location.x), phys.speed * sin(phys.location.y))
 
 proc newPhysics*(x: float, y: float): Physics =
-  var physics = new(Physics)
+  result = new(Physics)
 
-  physics.location.x = x
-  physics.location.y = y
+  result.location.x = x
+  result.location.y = y
 
-  result = physics
-
-############ Particle ##########################
 type
   Life* = ref object of RootObj
     IsAlive*: bool
@@ -51,16 +39,14 @@ proc update*(life: Life) =
     if life.Age >= life.Ttl:
       life.IsAlive = false
     else:
-      life.Age = life.Age + 1
+      life.Age += 1
 
 proc newLife*(alive: bool; ttl: int): Life =
-  let life: Life = new(Life)
+  result = new(Life)
 
-  life.IsAlive = alive
-  life.Age     = 0
-  life.Ttl     = ttl
-
-  result = life
+  result.IsAlive = alive
+  result.Age     = 0
+  result.Ttl     = ttl
 
 type
   Particle* = ref object of RootObj
@@ -78,27 +64,26 @@ proc update*(particle: Particle) =
   if particle.life.IsAlive:
     particle.physics.update
     particle.sprite.position = particle.physics.location
-    particle.sprite.color = color(particle.sprite.color.r,  particle.sprite.color.g, particle.sprite.color.b, uint8(float(particle.sprite.color.a) - (100/ particle.life.Ttl)))
+    particle.sprite.color    = color(particle.sprite.color.r,  particle.sprite.color.g, particle.sprite.color.b, uint8(float(particle.sprite.color.a) - (100/ particle.life.Ttl)))
     
-    var
-      size = particle.sprite.scale.x - (1 / particle.life.Ttl)
+    let size = particle.sprite.scale.x - (1 / particle.life.Ttl)
 
     particle.sprite.scale = vec2(size, size)
 
 
 proc newParticle*(texture: Texture, x: float; y: float): Particle =
   let
-    particle: Particle = new(Particle)
-    sprite:   Sprite   =   new_Sprite(texture)
-    size               = texture.size
+    sprite: Sprite = new_Sprite(texture)
+    size           = texture.size
 
-  sprite.origin    = vec2(size.x/2, size.x/2)
-  sprite.scale     = vec2(0.125, 0.125)
-  particle.physics = newPhysics(x, y)
-  sprite.position  = particle.physics.location
-  particle.life    = newLife(true, 255)
-  particle.sprite  = sprite
-  result           = particle
+  result = new(Particle)
+
+  sprite.origin   = vec2(size.x/2, size.x/2)
+  sprite.scale    = vec2(0.125, 0.125)
+  result.physics  = newPhysics(x, y)
+  sprite.position = result.physics.location
+  result.life     = newLife(true, 255)
+  result.sprite   = sprite
 
 type
   ParticlePool* = ref object of RootObj
@@ -107,8 +92,7 @@ type
 
 proc grow(particlePool: ParticlePool, by: int) =
     for i in 1..by:
-      let
-        particle: Particle = newParticle(particlePool.texture, 0, 0)
+      let particle: Particle = newParticle(particlePool.texture, 0, 0)
 
       particle.life.Ttl     = 255
       particle.life.Age     = 0
@@ -117,20 +101,18 @@ proc grow(particlePool: ParticlePool, by: int) =
       particlePool.pool.add(particle)
 
 proc borrow*(pool: ParticlePool, x: float, y: float, color: Color, ttl: int, speed: float, rotation: float): Particle =
-  if len(pool.pool) == 0:
-    pool.grow(1000)
+  if len(pool.pool) == 0: pool.grow(1000)
 
   result = pool.pool.pop
 
   result.physics.location = vec2(x, y)
   result.physics.rotation = rotation
-  result.physics.speed = speed
-  result.life.Age     = 0
-  result.life.Ttl     = ttl
-  result.life.IsAlive = true
-
-  result.sprite.color = color
-  result.sprite.scale = vec2(0.5, 0.5)
+  result.physics.speed    = speed
+  result.life.Age         = 0
+  result.life.Ttl         = ttl
+  result.life.IsAlive     = true
+  result.sprite.color     = color
+  result.sprite.scale     = vec2(0.5, 0.5)
 
 proc ret*(pool: ParticlePool, particle: Particle) =
   pool.pool.add(particle)
@@ -182,8 +164,6 @@ proc update*(emitter: Emitter) =
       emitter.pool.ret(particle)
       emitter.particles.delete(i)
   
-  
-
   if emitter.curParticles < emitter.maxParticles:
     for i in 1..min(50, emitter.maxParticles - emitter.curParticles):
       var
@@ -207,15 +187,11 @@ proc newEmitter*(
 
   result = new(Emitter)
 
-  result.twister = newMersenneTwister(int(epochTime()))
-
-
-  result.physics = newPhysics(x, y)
-
+  result.twister      = newMersenneTwister(int(epochTime()))
+  result.physics      = newPhysics(x, y)
   result.maxParticles = maxParticles
   result.curParticles = 0
   result.particles    = @[]
-
   result.pool         = pool
   result.speed        = speed
   result.rotation     = rotation
@@ -223,4 +199,3 @@ proc newEmitter*(
   result.color        = color
   result.alpha        = alpha
   result.ttl          = ttl
-  
--- flare_demo.nim
+++ flare_demo.nim
@@ -1,42 +1,23 @@
-import csfml
-import flare
-import mersenne
-import sequtils
+import csfml, flare, mersenne, sequtils
 
 const
-  WINDOW_TITLE  = "Flare"
-  WINDOW_WIDTH  = 1366
-  WINDOW_HEIGHT = 768
-  MAX_FIX_X = 400
-  MAX_FIX_Y = 200
-  MIN_FIX_X = 200
-  MIN_FIX_Y = 100
-
-  SCALE_FACTOR = 0.01
-  MAX_SCALE = 25
-  MIN_SCALE = 0.25
-
-  MAX_PART_SPAWN_VAR = 125
-  MAX_VEL = 800
-  VEL_SCALE_FACTOR = 0.0025
-  FULL_OPACITY = 255
-  NUM_PARTICLES = 800
-
-  PARTICLE_IMG = "resources/1.png"
+  WINDOW_TITLE     = "Flare"
+  WINDOW_WIDTH     = 1366
+  WINDOW_HEIGHT    = 768
+  PARTICLE_IMG     = "resources/1.png"
+  BACKGROUND_COLOR = color(0, 0, 0, 255)
 
 let
   window           = new_RenderWindow(video_mode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE)
   texture          = new_texture(PARTICLE_IMG)
   particlePool     = newParticlePool(texture)
-  SPAWN_COLOR      = color(0, 0, 255, 255)
-  BACKGROUND_COLOR = color(0, 0, 0, 255)
-  prop = newProperty(2.7, 10, 2.9)
-  speed = newProperty(10.0, 10, 0.7)
-  ttl = newProperty(10.0, 10, 10.5)
+  prop             = newProperty(2.7, 10, 2.9)
+  speed            = newProperty(10.0, 10, 0.7)
+  ttl              = newProperty(10.0, 10, 10.5)
   emitter: Emitter = newEmitter(particlePool, 400.0, 400.0, speed, prop, prop, prop, prop, ttl, 400)
 var
-  particles: seq[Particle] = @[]
-  twister: MersenneTwister = newMersenneTwister(1)
+  particles: seq[Particle]   = @[]
+  twister:   MersenneTwister = newMersenneTwister(1)
 
 window.vertical_sync_enabled = true
 
