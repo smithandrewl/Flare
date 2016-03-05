7abe668557b3e5f4827a5594137c14f2dc68ca2c
Andrew Smith <smith.andrew.l@gmail.com>
2016-03-04 21:55:09 -0600

    Code cleanup

--- flare_demo.nim
+++ flare_demo.nim
@@ -17,9 +17,6 @@ let
   particlePool2     = newParticlePool(texture2)
 
   prop             = newProperty(2.7, 10, 2.9)
-  speed            = newProperty(5, 10, 0.7)
-  ttl              = newProperty(4.0, 10, 10.5)
-  rotation         = newProperty(10.0, 10, 30.0)
 
   emitter: Emitter = newEmitter(
     particlePool, 
@@ -51,16 +48,16 @@ window.vertical_sync_enabled = true
 
 emitter.physics.rotation = 0
 
-var font: Font = new_Font("resources/DroidSansMono/DroidSansMono.ttf")
-var text: Text = new_Text("", font)
+var font:  Font = new_Font("resources/DroidSansMono/DroidSansMono.ttf")
+var text:  Text = new_Text("", font)
 var text2: Text = new_Text("", font)
 var text3: Text = new_Text("Press Q to quit", font)
 
 text3.position = vec2(600, 10)
 text2.position = vec2(5, 45)
-text.position = vec2(5, 25)
+text.position  = vec2(5, 25)
 
-text.characterSize = 16
+text.characterSize  = 16
 text2.characterSize = 16
 text3.characterSize = 16
 
@@ -77,7 +74,7 @@ while window.open:
                 window.close()
               else: discard
           else:
-            emitter.physics.location = mouse_getPosition()
+            emitter.physics.location  = mouse_getPosition()
             emitter2.physics.location = vec2(mouse_getPosition().x + 500, mouse_getPosition().y)
 
     window.clear BACKGROUND_COLOR
@@ -88,10 +85,12 @@ while window.open:
     emitter.draw(window)
     emitter2.draw(window)
 
-    let particleCount = len(emitter.particles) + len(emitter2.particles)
+    let particleCount   = len(emitter.particles) + len(emitter2.particles)
     let pooledParticles = len(emitter.pool.pool) + len(emitter2.pool.pool)
-    text.str = "Active Particles: " & $particleCount
+    
+    text.str  = "Active Particles: " & $particleCount
     text2.str = "Pooled particles: " & $pooledParticles
+    
     window.draw(text)
     window.draw(text2)
     window.draw(text3)
