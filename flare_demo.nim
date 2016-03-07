8e5062335a94dad50b5a3c07199a3f219b7b4ad9
Andrew Smith <smith.andrew.l@gmail.com>
2016-03-07 15:14:40 -0600

    Can now control emitters individually

--- flare_demo.nim
+++ flare_demo.nim
@@ -22,8 +22,8 @@ let
 
   emitter: Emitter = newEmitter(
     pool = particlePool, 
-    x            = 0, 
-    y            = 0, 
+    x            = 400, 
+    y            = 400, 
     speed        = newProperty(5, 10, 0.7), 
     rotation     = newProperty(10.0, 10, 30.0), 
     size         = newProperty(0.25, 10, 0.25), 
@@ -35,8 +35,8 @@ let
   
   emitter2: Emitter = newEmitter(
     pool = particlePool2, 
-    x            = 0, 
-    y            = 0, 
+    x            = 800, 
+    y            = 400, 
     speed        = newProperty(2.0, 10, 0.5), 
     rotation     = newProperty(3.0, 10, 2.0), 
     size         = newProperty(0.25, 10, 0.25), 
@@ -48,8 +48,8 @@ let
 
   emitter3: Emitter = newEmitter(
     pool = particlePool3, 
-    x            = 0, 
-    y            = 0, 
+    x            = 1200, 
+    y            = 400, 
     speed        = newProperty(1.5, 10, 5.125), 
     rotation     = newProperty(1.5, 10, 0.0125), 
     size         = newProperty(0.125, 10, 0.5), 
@@ -59,6 +59,8 @@ let
     maxParticles = maxParticles
   )
 
+var activeEmitter: Emitter = nil
+
 window.vertical_sync_enabled = true
 
 emitter.physics.rotation = 0
@@ -66,9 +68,9 @@ emitter.physics.rotation = 0
 var font:  Font = new_Font("resources/DroidSansMono/DroidSansMono.ttf")
 var text:  Text = new_Text("", font)
 var text2: Text = new_Text("", font)
-var text3: Text = new_Text("Press Q to quit", font)
+var text3: Text = new_Text("Press Q to quit, 1-3 to control emitters, 4 to detach", font)
 
-text3.position = vec2(600, 10)
+text3.position = vec2(400, 10)
 text2.position = vec2(5, 45)
 text.position  = vec2(5, 25)
 
@@ -85,13 +87,23 @@ while window.open:
             window.close()
           of EventType.KeyPressed:
             case event.key.code
+              of KeyCode.Num1:
+                activeEmitter = emitter
+                mouse_setPosition(vec2(int(activeEmitter.physics.location.x), int(activeEmitter.physics.location.y)))
+              of KeyCode.Num2:
+                activeEmitter = emitter2
+                mouse_setPosition(vec2(int(activeEmitter.physics.location.x), int(activeEmitter.physics.location.y)))
+              of KeyCode.Num3:
+                activeEmitter =  emitter3
+                mouse_setPosition(vec2(int(activeEmitter.physics.location.x), int(activeEmitter.physics.location.y)))
+              of KeyCode.Num4:
+                activeEmitter = nil
               of KeyCode.Q:
                 window.close()
               else: discard
           else:
-            emitter.physics.location  = mouse_getPosition()
-            emitter2.physics.location = vec2(mouse_getPosition().x + 400, mouse_getPosition().y)
-            emitter3.physics.location = vec2(mouse_getPosition().x + 800, mouse_getPosition().y)
+            if activeEmitter != nil:
+              activeEmitter.physics.location  = mouse_getPosition()
     window.clear BACKGROUND_COLOR
 
     emitter.update
