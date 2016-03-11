5cba609a52ccb6530c0f6f6361a13b18bdd0c75c
Andrew Smith <smith.andrew.l@gmail.com>
2016-03-10 22:19:38 -0600

    Added ParticlePool test suite

--- flare_test.nim
+++ flare_test.nim
@@ -1,13 +1,14 @@
 import unittest
 import flare
+import csfml
 
 suite "Life tests":
-  test "Life: Constructor sets values":
+  test "Life: Constructor":
     let life = newLife(false, 20)
 
-    require(life.Age == 0)
+    require(life.Age     == 0)
     require(life.IsAlive == false)
-    require(life.Ttl == 20)
+    require(life.Ttl     == 20)
 
   test "Life: Age updates by one each step":
     let life: Life = newLife(true, 110)
@@ -32,5 +33,15 @@ suite "Life tests":
     require(life.IsAlive == false)
 
     life.update
-  
-  
+
+suite "ParticlePool tests":
+  setup:
+    let
+      texture      = new_texture("resources/1.png") 
+      particlePool = newParticlePool(texture)
+
+  test "ParticlePool: Constructor":
+    require(particlePool.texture == texture)
+
+  test "ParticlePool: Initializes with 500 particles in the pool":
+    require(particlePool.pool.len == 500)
